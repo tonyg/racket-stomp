1cf866eb9f4b006ad51b88d0d66d2acbb97240af
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2012-02-09 11:31:09 -0500

    Fix incorrect construction of exn:fail:read:eof.

--- session.rkt
+++ session.rkt
@@ -102,7 +102,8 @@
     (if (eof-object? f)
 	(raise (exn:fail:read:eof
 		(format "Disconnected before receipt ~s was received" receipt)
-		(current-continuation-marks)))
+		(current-continuation-marks)
+		'()))
 	#t)))
 
 (define (make-counter prefix initial-value)
