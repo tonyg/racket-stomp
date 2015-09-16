de66e4efff712f8d6d669b1a3e6be6658d5fa797
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2015-09-15 22:41:20 -0400

    Move examples into (module+ main ...) to avoid running them during raco test.

--- stomp/examples/rpc-client.rkt
+++ stomp/examples/rpc-client.rkt
@@ -1,47 +1,49 @@
 #lang racket
 
-(require "../main.rkt") ;; or just (require stomp)
+(module+ main
+  (require "../main.rkt") ;; or just (require stomp)
 
-;; The program takes one mandatory positional argument: the name of a
-;; person to greet.
-(define name-to-greet
-  (command-line
-   #:program "rpc-client.rkt"
-   #:args (name-to-greet)
-   name-to-greet))
+  ;; The program takes one mandatory positional argument: the name of a
+  ;; person to greet.
+  (define name-to-greet
+    (command-line
+     #:program "rpc-client.rkt"
+     #:args (name-to-greet)
+     name-to-greet))
 
-;; We wrap the code in a handler for exn:stomp, in order to get
-;; insight into the details of any error messages returned by the
-;; server.
-(with-handlers ([exn:stomp?
-                 (lambda (e)
-                   (pretty-print e))])
-  (define s (stomp-connect "dev.rabbitmq.com"
-                           #:login "guest"
-                           #:passcode "guest"
-                           #:virtual-host "/"))
+  ;; We wrap the code in a handler for exn:stomp, in order to get
+  ;; insight into the details of any error messages returned by the
+  ;; server.
+  (with-handlers ([exn:stomp?
+                   (lambda (e)
+                     (pretty-print e))])
+    (define s (stomp-connect "dev.rabbitmq.com"
+                             #:login "guest"
+                             #:passcode "guest"
+                             #:virtual-host "/"))
 
-  ;; We send the request to the name associated with the RPC service
-  ;; from rpc-server.rkt. The body of the message is just the name of
-  ;; the person to greet from the command line.
-  ;;
-  ;; We make sure to include a "reply-to" header containing the name
-  ;; of a temporary queue. When the server replies, it will cause a
-  ;; MESSAGE frame to be delivered to us labelled with the name of the
-  ;; temporary queue as its *subscription id*. For details of the way
-  ;; RabbitMQ uses temporary queues, please see their documentation at
-  ;; https://www.rabbitmq.com/stomp.html
-  (stomp-send s "/amq/queue/rpc-server.rkt" (string->bytes/utf-8 name-to-greet)
-              #:headers '((reply-to "/temp-queue/my-replies")))
+    ;; We send the request to the name associated with the RPC service
+    ;; from rpc-server.rkt. The body of the message is just the name of
+    ;; the person to greet from the command line.
+    ;;
+    ;; We make sure to include a "reply-to" header containing the name
+    ;; of a temporary queue. When the server replies, it will cause a
+    ;; MESSAGE frame to be delivered to us labelled with the name of the
+    ;; temporary queue as its *subscription id*. For details of the way
+    ;; RabbitMQ uses temporary queues, please see their documentation at
+    ;; https://www.rabbitmq.com/stomp.html
+    (stomp-send s "/amq/queue/rpc-server.rkt" (string->bytes/utf-8 name-to-greet)
+                #:headers '((reply-to "/temp-queue/my-replies")))
 
-  ;; This line waits for the reply MESSAGE frame to appear on the
-  ;; correct temporary queue subscription. Note that the temp-queue
-  ;; labels used only have to be unique on a per-connection basis --
-  ;; they don't have to be globally unique.
-  (define reply (stomp-next-message s "/temp-queue/my-replies"))
+    ;; This line waits for the reply MESSAGE frame to appear on the
+    ;; correct temporary queue subscription. Note that the temp-queue
+    ;; labels used only have to be unique on a per-connection basis --
+    ;; they don't have to be globally unique.
+    (define reply (stomp-next-message s "/temp-queue/my-replies"))
 
-  ;; We simply print the MESSAGE we got from the server...
-  (pretty-print reply)
+    ;; We simply print the MESSAGE we got from the server...
+    (pretty-print reply)
 
-  ;; ... and disconnect.
-  (stomp-disconnect s))
+    ;; ... and disconnect.
+    (stomp-disconnect s)))
+
--- stomp/examples/stomp-tail.rkt
+++ stomp/examples/stomp-tail.rkt
@@ -1,35 +1,36 @@
 #lang racket
 
-(require "../main.rkt") ;; or just (require stomp)
+(module+ main
+  (require "../main.rkt") ;; or just (require stomp)
 
-(with-handlers ([exn:stomp?
-                 (lambda (e)
-                   (pretty-print e))])
-  (define s (stomp-connect "dev.rabbitmq.com"
-                           #:login "guest"
-                           #:passcode "guest"
-                           #:virtual-host "/"))
-
-  (stomp-subscribe s "/exchange/amq.rabbitmq.log/#" "s1")
-  (stomp-subscribe s "/exchange/amq.direct/stomp-tail" "s2")
-
-  (define (w fmt . args)
-    (stomp-send s "/exchange/amq.direct/stomp-tail"
-                (string->bytes/utf-8 (apply format fmt args))
-                #:headers `((content-type "application/octet-stream"))))
-
-  (w "Here I am! My session is ~v" (stomp-session-id s))
-  (with-handlers ([exn:break?
+  (with-handlers ([exn:stomp?
                    (lambda (e)
-                     (w "Goodbye! from ~v" (stomp-session-id s))
-                     (stomp-unsubscribe s "s1")
-                     (stomp-unsubscribe s "s2")
-                     (stomp-disconnect s)
-                     (pretty-print 'clean-disconnect-complete)
-                     (raise e))])
-    (let loop ()
-      (let ((m (stomp-next-message s (set "s1" "s2"))))
-        (when (not (eof-object? m))
-          (pretty-print m)
-          (loop))))
-    (pretty-print 'broker-went-away)))
+                     (pretty-print e))])
+    (define s (stomp-connect "dev.rabbitmq.com"
+                             #:login "guest"
+                             #:passcode "guest"
+                             #:virtual-host "/"))
+
+    (stomp-subscribe s "/exchange/amq.rabbitmq.log/#" "s1")
+    (stomp-subscribe s "/exchange/amq.direct/stomp-tail" "s2")
+
+    (define (w fmt . args)
+      (stomp-send s "/exchange/amq.direct/stomp-tail"
+                  (string->bytes/utf-8 (apply format fmt args))
+                  #:headers `((content-type "application/octet-stream"))))
+
+    (w "Here I am! My session is ~v" (stomp-session-id s))
+    (with-handlers ([exn:break?
+                     (lambda (e)
+                       (w "Goodbye! from ~v" (stomp-session-id s))
+                       (stomp-unsubscribe s "s1")
+                       (stomp-unsubscribe s "s2")
+                       (stomp-disconnect s)
+                       (pretty-print 'clean-disconnect-complete)
+                       (raise e))])
+      (let loop ()
+        (let ((m (stomp-next-message s (set "s1" "s2"))))
+          (when (not (eof-object? m))
+            (pretty-print m)
+            (loop))))
+      (pretty-print 'broker-went-away))))
